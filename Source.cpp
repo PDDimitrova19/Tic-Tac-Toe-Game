b63e90de973f0c4912c88337eda98efffd14fd0a
PDDimitrova19 <63718738+PDDimitrova19@users.noreply.github.com>
2020-11-19 18:51:03 +0200

    Finish code for Player 2 in game between 2 players

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 void GameName() {
-    cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
-    cout << endl;
-    cout << "      ///////     ////////     ////////          ///////     ////     ///////" << endl;
-    cout << "        //          //        //                  //       //  //    //" << endl;
-    cout << "       //          //        //                  //      //   //    //" << endl;
-    cout << "      //          //        //                  //      ////////   //   " << endl;
-    cout << "     //       ////////     ////////            //     //     //   ///////" << endl;
-    cout << endl;
-    cout << "                    ///////    //////      ///////" << endl;
-    cout << "                     //      //    //     //" << endl;
-    cout << "                    //      //    //     /////// " << endl;
-    cout << "                   //      //    //     //" << endl;
-    cout << "                  //       //////      ////////" << endl;
-    cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
-
+	cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+	cout << endl;
+	cout << "    ///////    /////////     ////////         ////////     ////     ///////" << endl;
+	cout << "      //          //        //                  //       //  //    //" << endl;
+	cout << "     //          //        //                  //      //   //    //" << endl;
+	cout << "    //          //        //                  //     ////////    //   " << endl;
+	cout << "   //       ////////     ////////            //    //     //    ///////" << endl;
+	cout << endl;
+	cout << "                       ///////     //////      ///////" << endl;
+	cout << "                         //      //    //     //" << endl;
+	cout << "                        //      //    //     /////// " << endl;
+	cout << "                       //      //    //     //" << endl;
+	cout << "                      //       //////      ////////" << endl;
+	cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " << endl;
+	cout << endl;
 }
 char GameBoard(char GameBoardPlaces[]) {
 	cout << endl;
@@ -46,6 +46,13 @@ void GameBetween2Players() {
 			((GameBoardPlaces[1] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[7]) && GameBoardPlaces[1] == 'X') || ((GameBoardPlaces[2] == GameBoardPlaces[5]) && (GameBoardPlaces[5] == GameBoardPlaces[8]) && GameBoardPlaces[2] == 'X') ||
 			((GameBoardPlaces[6] == GameBoardPlaces[7]) && (GameBoardPlaces[7] == GameBoardPlaces[8]) && GameBoardPlaces[6] == 'X') || ((GameBoardPlaces[3] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[5]) && GameBoardPlaces[3] == 'X') || ((GameBoardPlaces[2] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[6]) && GameBoardPlaces[2] == 'X')) {
 			cout << "\n                       !!!  Player 1 wins  !!!" << endl;
+			winnerCounter = 1;
+			break;
+		}
+		else if (((GameBoardPlaces[0] == GameBoardPlaces[1]) && (GameBoardPlaces[1] == GameBoardPlaces[2]) && GameBoardPlaces[0] == 'O') || ((GameBoardPlaces[0] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[8]) && GameBoardPlaces[0] == 'O') || ((GameBoardPlaces[0] == GameBoardPlaces[3]) && (GameBoardPlaces[3] == GameBoardPlaces[6]) && GameBoardPlaces[0] == 'O') ||
+			((GameBoardPlaces[1] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[7]) && GameBoardPlaces[1] == 'O') || ((GameBoardPlaces[2] == GameBoardPlaces[5]) && (GameBoardPlaces[5] == GameBoardPlaces[8]) && GameBoardPlaces[2] == 'O') ||
+			((GameBoardPlaces[6] == GameBoardPlaces[7]) && (GameBoardPlaces[7] == GameBoardPlaces[8]) && GameBoardPlaces[6] == 'O') || ((GameBoardPlaces[3] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[5]) && GameBoardPlaces[3] == 'O') || ((GameBoardPlaces[2] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[6]) && GameBoardPlaces[2] == 'O')) {
+			cout << "\n                       !!!  Player 2 wins  !!!" << endl;
 			winnerCounter = 1;
 			break;
 		}
@@ -180,12 +187,143 @@ void GameBetween2Players() {
 		if (winnerCounter == 1) {
 			break;
 		}
+		cout << "\n                     Player 2, enters a number : ";
+		cin >> Player2;
+		int CaseDefault2 = 0;
+		for (int j = 0; j < 100; j++) {
+			if (CaseDefault2 == 1) {
+				cout << endl;
+				cout << "\n                       !!!  Incorrect Input  !!!" << endl;
+				cout << "           Try again and enter a valid number from the board\n" << endl;
+				GameBoard(GameBoardPlaces);
+				cout << "                     Player 2, enters a number : ";
+				cin >> Player2;
+				CaseDefault2 = 0;
+			}
+			switch (Player2) {
+			case 1: {
+				if (GameBoardPlaces[0] == '1') {
+					GameBoardPlaces[0] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 2: {
+				if (GameBoardPlaces[1] == '2') {
+					GameBoardPlaces[1] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 3: {
+				if (GameBoardPlaces[2] == '3') {
+					GameBoardPlaces[2] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 4: {
+				if (GameBoardPlaces[3] == '4') {
+					GameBoardPlaces[3] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 5: {
+				if (GameBoardPlaces[4] == '5') {
+					GameBoardPlaces[4] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 6: {
+				if (GameBoardPlaces[5] == '6') {
+					GameBoardPlaces[5] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 7: {
+				if (GameBoardPlaces[6] == '7') {
+					GameBoardPlaces[6] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 8: {
+				if (GameBoardPlaces[7] == '8') {
+					GameBoardPlaces[7] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			case 9: {
+				if (GameBoardPlaces[8] == '9') {
+					GameBoardPlaces[8] = 'O';
+					GameBoard(GameBoardPlaces);
+					Counter++;
+				}
+				else {
+					CaseDefault2++;
+				}
+				break;
+			}
+			default: {
+				CaseDefault2++;
+				break;
+			}
+			}
+			if (CaseDefault2 == 0) {
+				if (((GameBoardPlaces[0] == GameBoardPlaces[1]) && (GameBoardPlaces[1] == GameBoardPlaces[2]) && GameBoardPlaces[0] == 'O') || ((GameBoardPlaces[0] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[8]) && GameBoardPlaces[0] == 'O') || ((GameBoardPlaces[0] == GameBoardPlaces[3]) && (GameBoardPlaces[3] == GameBoardPlaces[6]) && GameBoardPlaces[0] == 'O') ||
+					((GameBoardPlaces[1] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[7]) && GameBoardPlaces[1] == 'O') || ((GameBoardPlaces[2] == GameBoardPlaces[5]) && (GameBoardPlaces[5] == GameBoardPlaces[8]) && GameBoardPlaces[2] == 'O') ||
+					((GameBoardPlaces[6] == GameBoardPlaces[7]) && (GameBoardPlaces[7] == GameBoardPlaces[8]) && GameBoardPlaces[6] == 'O') || ((GameBoardPlaces[3] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[5]) && GameBoardPlaces[3] == 'O') || ((GameBoardPlaces[2] == GameBoardPlaces[4]) && (GameBoardPlaces[4] == GameBoardPlaces[6]) && GameBoardPlaces[2] == 'O')) {
+					cout << "\n                       !!!  Player 2 wins  !!!" << endl;
+					winnerCounter = 1;
+				}
+				break;
+			}
+		}
+		if (winnerCounter == 1) {
+			break;
+		}
 	}
-
 }
 void GameBetweenComputerAnd1Player() {
 	cout << "\n" << "                   Player 1 (X) ------- Computer (O)" << endl;
-	GameBoard();
+	char GameBoardPlaces[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	GameBoard(GameBoardPlaces);
 }
 int main()
 {
